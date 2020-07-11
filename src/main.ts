57a7af4d778f2f5947e539ebe9a6b2bd75874fe0
Tsuyoshi Miyake <tsuyoshi.miyake@gmail.com>
2020-07-11 22:39:18 +0900

    finish "Parsing the Event Data"

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import * as core from '@actions/core'
-// import {wait} from './wait'
+import * as event from './event'
+import * as version from './version'
 
 export async function run(): Promise<void> {
   try {
-    // const ms: string = core.getInput('milliseconds')
-    // core.debug(`Waiting ${ms} milliseconds ...`)
+    const tag = event.getCreatedTag()
 
-    // core.debug(new Date().toTimeString())
-    // await wait(parseInt(ms, 10))
-    // core.debug(new Date().toTimeString())
+    if (tag && version.isSemVer(tag)) {
+      // TODO
+    }
 
-    // core.setOutput('time', new Date().toTimeString())
     core.setOutput('release-url', 'https://example.com')
   } catch (error) {
     core.setFailed(error.message)
