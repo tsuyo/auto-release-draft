3f010309f5a6c6cf038951de1dc16b2b81aa9efe
Tsuyoshi Miyake <tsuyoshi.miyake@gmail.com>
2020-07-12 00:15:33 +0900

    finish "Running Command-line Programs"

---
+++ src/git.ts
@@ -0,0 +1,86 @@
+import * as core from '@actions/core'
+import { exec } from '@actions/exec'
+import { ExecOptions } from '@actions/exec/lib/interfaces'
+
+export async function getChangesIntroducedByTag(tag: string): Promise<string> {
+  const previousVersionTag = await getProviousVersionTag(tag)
+  
+  return previousVersionTag
+    ? getCommitMessagesBetween(previousVersionTag, tag)
+    : getCommitMessagesFrom(tag)
+}
+
+export async function getProviousVersionTag(tag: string): Promise<string | null> {
+  let previousTag = ''
+
+  const options: ExecOptions = {
+    listeners: {
+      stdout: (data: Buffer) => {
+        previousTag += data.toString()
+      }
+    },
+    silent: true,
+    ignoreReturnCode: true
+  }
+
+  const exitCode = await exec(
+    'git',
+    ['describe',
+    '--match', 'v[0-9]*',
+    '--abbrev=0',
+    '--first-parent',
+    `${tag}^`],
+    options)
+  
+    core.debug(`The previous version tag is ${previousTag}`)
+
+    return exitCode === 0 ? previousTag.trim() : null
+}
+
+export async function getCommitMessagesBetween(firstTag: string, secondTag: string): Promise<string> {
+  let commitMessages = ''
+
+  const options: ExecOptions = {
+    listeners: {
+      stdout: (data: Buffer) => {
+        commitMessages += data.toString()
+      }
+    },
+    silent: true
+  }
+
+  const exitCode = await exec(
+    'git',
+    ['log',
+    '--format=%s',
+    `${firstTag}..${secondTag}`],
+    options)
+  
+    core.debug(`The commit messages between ${firstTag} and ${secondTag} are:\n${commitMessages}`)
+
+    return commitMessages.trim()
+}
+
+export async function getCommitMessagesFrom(tag: string): Promise<string> {
+  let commitMessages = ''
+
+  const options: ExecOptions = {
+    listeners: {
+      stdout: (data: Buffer) => {
+        commitMessages += data.toString()
+      }
+    },
+    silent: true
+  }
+
+  const exitCode = await exec(
+    'git',
+    ['log',
+    '--format=%s',
+    tag],
+    options)
+  
+    core.debug(`The commit messages from ${tag} are:\n${commitMessages}`)
+
+    return commitMessages.trim()
+}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import * as core from '@actions/core'
 import * as event from './event'
 import * as version from './version'
+import * as git from './git'
 
 export async function run(): Promise<void> {
   try {
     const tag = event.getCreatedTag()
 
     if (tag && version.isSemVer(tag)) {
-      // TODO
+      const changelog = await git.getChangesIntroducedByTag(tag)
     }
 
     core.setOutput('release-url', 'https://example.com')
